2907c3740e819f88b5f3a91d8c9d7025671f17fc
peter-lelkes <p.lelkes@gmail.com>
2025-06-09 14:58:14 +0200

    metoden 'addNewCollection' skapad

--- src/app/services/app-service.service.ts
+++ src/app/services/app-service.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+
+import { Firestore, doc, setDoc } from '@angular/fire/firestore'; // 🔹 Lägg till denna import!
+import { nanoid } from 'nanoid';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
+  private firestore = inject(Firestore);
+
   constructor() {}
+
+  // 🔹 Metod för att lägga till en ny collection
+  async addNewCollection() {
+    const docRef = doc(this.firestore, 'workouts', nanoid(5));
+    await setDoc(docRef, { createdAt: new Date() });
+
+    console.log('🔥 Collection skapad!');
+  }
 }
